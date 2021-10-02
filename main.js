30629a9b4cffaf4b060179dba3dc542df1720b0e
sina42048 <sinafat76@gmail.com>
2021-10-02 20:20:22 +0330

    login operation implemented

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
-const { app, BrowserWindow } = require("electron");
+const { app, BrowserWindow, ipcMain, webContents } = require("electron");
+const { IgApiClient } = require("instagram-private-api");
 const url = require("url");
 const path = require("path");
 
@@ -6,12 +7,19 @@ let win;
 
 function createWindow() {
   win = new BrowserWindow({
+    center: true,
+    fullscreenable: false,
+    hasShadow: true,
+    title: "Instagram Unfollowers",
     width: 640,
     height: 480,
+    resizable: false,
+    //frame: false,
     webPreferences: {
-      preload: path.join(__dirname, path.sep, "scripts", "preload.js")
-    }
+      preload: path.join(__dirname, path.sep, "scripts", "preload.js"),
+    },
   });
+
   win.loadURL(
     url.format({
       pathname: path.join(__dirname, "index.html"),
@@ -19,9 +27,58 @@ function createWindow() {
       slashes: true,
     })
   );
+
   win.webContents.openDevTools();
 }
 
+app.setAppUserModelId("Instagram Unfollowers");
+
 app.whenReady().then(() => {
   createWindow();
+
+  ipcMain.on("login_request", async (event, args) => {
+    const userData = {
+      username: args[0].username,
+      password: args[0].password,
+    };
+    await instagram.loginRequest(
+      userData,
+      (user) => {
+        event.sender.send("login_response", [{ success: true, ...user }]);
+      },
+      (err) => {
+        event.sender.send("login_response", [{ success: false, err }]);
+      }
+    );
+  });
+
+  app.on("activate", () => {
+    if (BrowserWindow.getAllWindows().length === 0) {
+      createWindow();
+    }
+  });
 });
+
+app.on("window-all-closed", () => {
+  if (process.platform !== "darwin") {
+    app.quit();
+  }
+});
+
+const instagram = (function () {
+  const ig = new IgApiClient();
+
+  async function loginRequest({ username, password }, onSuccess, onFailure) {
+    ig.state.generateDevice((Math.random() * 100000).toString());
+    try {
+      const userData = await ig.account.login(username, password);
+      onSuccess(userData);
+    } catch (err) {
+      onFailure(err);
+    }
+  }
+
+  return {
+    loginRequest,
+  };
+})();
