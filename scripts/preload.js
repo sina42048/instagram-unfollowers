881d110ca155cc6d55de44be0885e586d3dfeaae
sina42048 <sinafat76@gmail.com>
2021-10-02 20:21:02 +0330

    ipcrenderer for login operation added to contextbridge

--- scripts/preload.js
+++ scripts/preload.js
@@ -2,5 +2,9 @@ const { ipcRenderer } = require("electron");
 const { contextBridge } = require("electron");
 
 contextBridge.exposeInMainWorld("api", {
-    ipcRenderer: ipcRenderer
+  loginRequest: (username, password) =>
+    ipcRenderer.send("login_request", [{ username, password }]),
+  onLoginResponse: (handler) => {
+    ipcRenderer.on("login_response", handler);
+  },
 });
